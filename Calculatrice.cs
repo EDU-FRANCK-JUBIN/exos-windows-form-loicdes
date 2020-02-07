d67ff9a9647d68353c040564127097ed5395e9af
loic <loic.des972@gmail.com>
2020-02-07 16:03:19 +0000

    Windows Form + calculatrice

---
+++ Calculatrice.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaSuperWindowsForm
+{
+    public partial class Calculatrice : Form
+    {
+        private static List<string> Operators = new List<string>();
+        private static List<string> Numbers = new List<string>() { "0" };
+        private static double Result;
+        private static int Step = 0;
+        public Calculatrice()
+        {
+            InitializeComponent();
+        }
+
+        private void handleClick(object sender, EventArgs e)
+        {
+            dynamic senderObject = sender;
+            switch (senderObject.Text)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case ",":
+                    Numbers[Step] += senderObject.Text;
+                    DisplayResult();
+                    break;
+                case "-":
+                case "+":
+                case "*":
+                case "/":
+                    Operators.Add(senderObject.Text);
+                    Numbers.Add("");
+                    Step++;
+                    DisplayResult();
+                    break;
+                case "C":
+                    Numbers.Clear();
+                    Numbers.Add("0");
+                    Step = 0;
+                    Operators.Clear();
+                    DisplayResult();
+                    break;
+                case "CE":
+                    Numbers[Step] = "";
+                    DisplayResult();
+                    break;
+                case "+-":
+                    CalculateResult();
+                    Result *= -1;
+                    richTextBox1.Text = Result.ToString();
+                    break;
+                case "=":
+                    CalculateResult();
+                    richTextBox1.Text = Result.ToString();
+                    break;
+            }
+        }
+        private void DisplayResult()
+        {
+            richTextBox1.Text = "";
+            for (var i = 0; i < Numbers.Count; i++)
+            {
+                richTextBox1.Text += Numbers[i] != "" ? Double.Parse(Numbers[i]).ToString() : "";
+                richTextBox1.Text += i < Operators.Count ? Operators[i] : "";
+
+            }
+        }
+        private void CalculateResult()
+        {
+            double temp;
+            string tempOperator;
+            Result = Double.Parse(Numbers[0]);
+            for (var i = 0; i < Numbers.Count; i++)
+            {
+                temp = i+1 < Numbers.Count ? Double.Parse(Numbers[i+1]) : 0;
+                tempOperator = i < Operators.Count ? Operators[i] : "";
+                switch (tempOperator)
+                {
+                    case "-":
+                        Result -= temp;
+                        break;
+                    case "+":
+                        Result += temp;
+                        break;
+                    case "*":
+                        Result *= temp;
+                        break;
+                    case "/":
+                        Result /= temp;
+                        break;
+                    case "":
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace MaSuperWindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(112, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(112, 89);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(119, 116);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(119, 139);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 206);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Salut salut";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
